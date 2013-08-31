bf4f5d7780091f6ee2c60165de3a9d6291aeb7b7
Bhavik Maneck <bmaneck@gmail.com>
2013-08-31 19:45:08 +1000

    first commit, popcorn class will be main class that will be used at runtime

---
+++ popcorn.java
@@ -0,0 +1,11 @@
+//A Very Simple Example
+
+class popcorn {
+  public static void main(String[] args){
+	for (String arg : args)
+	{
+		System.out.println(arg+"\n");
+	}
+    System.out.println("I'm a Simple Popcorn Program");
+  }
+}
